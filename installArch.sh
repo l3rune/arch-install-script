a017c84388b64145c7afe6d5ff0a886bf576fdd5
l3rune <84282498+l3rune@users.noreply.github.com>
2021-05-18 20:53:08 +0200

    Update installArch.sh

--- installArch.sh
+++ installArch.sh
@@ -17,21 +17,21 @@ parted -s $HD rm 4 &> /dev/null
 parted -s /dev/sda mklabel gpt
 
 # boot-partition
-parted -s /dev/sda mkpart primary fat32 1Mib 260MiB
-parted -s /dev/sda set 1 esp on
-parted -s /dev/sda set 1 boot on
+parted -s /dev/sda mkpart primary fat32 1Mib 260MiB 1>/dev/null
+parted -s /dev/sda set 1 esp on 1>/dev/null
+parted -s /dev/sda set 1 boot on 1>/dev/null
 
 
 # root-partition
-parted -s /dev/sda mkpart primary ext4 261Mib 5381MiB
+parted -s /dev/sda mkpart primary ext4 261Mib 5381MiB 1>/dev/null
 
 # home-partition
-parted -s /dev/sda mkpart primary ext4 5381MiB 100%
+parted -s /dev/sda mkpart primary ext4 5381MiB 100% 1>/dev/null
 
 # file systems
-mkfs.fat -F 32 /dev/sda1 # boot
-mkfs.ext4 /dev/sda2 # root
-mkfs.ext4 /dev/sda3 # home
+mkfs.fat -F 32 /dev/sda1 1>/dev/null # boot
+mkfs.ext4 /dev/sda2 1>/dev/null # root
+mkfs.ext4 /dev/sda3 1>/dev/null # home
 
 # mount root
 mount /dev/sda2 /mnt
@@ -45,6 +45,7 @@ mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
 # pacstrap
+echo "[ PACTSRAP ]"
 pacstrap /mnt base base-devel vim nteworkmanager grub
 
 # fstsb
